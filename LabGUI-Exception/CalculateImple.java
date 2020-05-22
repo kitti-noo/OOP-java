3ee83c58b554caf44fd45482844ae09769e8cf5d
kla004 <fiw56789@gmail.com>
2020-05-22 21:08:52 +0700

    Implements

---
+++ LabGUI-Exception/CalculateImple.java
@@ -0,0 +1,98 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+//Implements
+
+public class CalculateImple extends JFrame implements ActionListener {
+    private JTextField num1 = new JTextField(10);
+    private JTextField num2 = new JTextField(10);
+    private JTextArea text = new JTextArea();
+    private JButton butadd = new JButton("Add");
+    private JButton butsub = new JButton("Subtract");
+    private JButton butclr = new JButton("Clear");
+    private Integer n1;
+    private Integer n2;
+    private int ans;
+
+    public CalculateImple (){
+        //GUI
+        Container c = getContentPane();
+        JPanel pa1 = new JPanel(new FlowLayout());
+        pa1.add(num1);
+        pa1.add(num2);
+
+        JPanel pa2 = new JPanel(new FlowLayout());
+        pa2.add(butadd);
+        pa2.add(butsub);
+        pa2.add(butclr);
+
+        c.setLayout(new GridLayout(3,1));
+        c.add(pa1);
+        c.add(pa2);
+        c.add(text);
+        setTitle("Calculate");
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);  
+        setSize(350,250);
+        pack();
+
+        //Event 
+        
+        butadd.addActionListener(this);
+        butsub.addActionListener(this);
+        butclr.addActionListener(this);
+
+        
+    }
+    public void actionPerformed(ActionEvent e){
+        try {
+            if (e.getSource() == butclr){
+                text.setText("");  
+                num1.setText("");
+                num2.setText("");
+            }
+            else {
+                Inputfill();
+                n1 = Integer.parseInt(num1.getText());
+                n2 = Integer.parseInt(num2.getText());
+
+                if (e.getSource() == butadd){
+                ans = n1+n2;
+                String t1 = n1+" + "+n2+" = "+ String.valueOf(ans);
+                text.append(t1 +"\n");
+                }
+                else if (e.getSource() == butsub) {
+                ans = n1-n2;
+                String t2 = n1+" - " +n2+" = "+ String.valueOf(ans);
+                text.append(t2 +"\n");
+                }
+            }
+            
+        }
+        catch (NumberFormatException nfe){
+            JOptionPane.showMessageDialog(null, "You must enter two integers", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+        catch (InputException ex){
+            JOptionPane.showMessageDialog(null, "Field missing", "Warning", JOptionPane.WARNING_MESSAGE);
+        }
+
+    }
+
+     //ExceptionClass
+     public class InputException extends ArithmeticException {
+        public InputException (){
+            super("Warning Field missing");
+        }
+    }
+
+    //Field missing
+    public void Inputfill() throws InputException {
+        if (num1.getText().length() == 0 || num2.getText().length() == 0) {
+            throw new InputException();
+        }
+    }
+
+    public static void main(String[] args) {
+        new CalculateImple();
+    }
+}
