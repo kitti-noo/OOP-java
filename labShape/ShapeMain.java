f3a5b4403ff8a7136bbc544e5289e257dc4308ae
kla004 <fiw56789@gmail.com>
2020-05-25 16:21:18 +0700

    Inheritance

--- labShape/ShapeMain.java
+++ labShape/ShapeMain.java
@@ -5,6 +5,7 @@ public class ShapeMain {
         shapes[0] = new Rectangular("A", 5.0, 10.0);
         shapes[1] = new Circle("B", 3.0);
         shapes[2] = new Circle("C", 4.0);
+        
         for (int i = 0; i < 3; i++) {
             System.out.println("Area of Shape " + (i+1) +" : " +shapes[i].getArea());
         }
